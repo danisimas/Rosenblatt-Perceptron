de5ca59a04224add59b7a222753538b8b925e43d
FMABr <felipemuamorim@gmail.com>
2024-05-21 15:35:30 -0400

    feat: read file data into usable format

---
+++ utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def read_data(filename: str):
+    # Read data from file
+    data = np.fromfile(f"./task/data/{filename}.txt")
+
+    # Separate values into (x1 x2 y) elements
+    data = data.reshape(-1, 3)
+
+    # Reorganize valkues into [([x1.1,x1.2], y1), ([x2.1,x2.2], y2)]
+    data = np.array([(row[:2], row[2]) for row in data], dtype=object)
+
+    return data
